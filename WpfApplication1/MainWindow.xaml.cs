3977e65be40b9113074410fafccb34759feddf87
ASUS\USER <raer1332@gmail.com>
2018-06-20 23:58:24 +0800

    no message

---
+++ WpfApplication1/MainWindow.xaml.cs
@@ -0,0 +1,93 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace accounting
+{
+    /// <summary>
+    /// MainWindow.xaml 的互動邏輯
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+        
+        private void Window_Loaded(object sender, RoutedEventArgs e)
+        {
+
+            string[] lines = { };
+            // 如果已有檔案,讀取他
+            try
+            {
+                lines = System.IO.File.ReadAllLines(@"D:\accountingApp\data.txt");
+            }
+            // 如果沒有檔案,新增一個
+            catch
+            {
+                List<string> standard = new List<string>();
+            }
+
+            // 解析每一行
+            foreach (string line in lines)
+            {
+                // 用 | 隔開不同資料
+                string[] parts = line.Split('|');
+
+                // 建立 category
+                category categorys = new category();
+
+                // 讀取category的資料
+                categorys.categoryDate.Text = parts[0];
+                categorys.categoryName.Text = parts[1];
+                categorys.categoryPrice.Text = parts[2];
+
+                // 放到清單列表中
+                categoryList.Children.Add(categorys);
+            }
+        }
+        // 按下+鍵時
+        private void addBtn_MouseDown(object sender, MouseButtonEventArgs e)
+        {
+            // 產生項目
+            category categorys = new category();
+
+            //放到清單中
+            categoryList.Children.Add(categorys);
+        }
+
+        // 關閉程式時
+        private void windowClosed(object sender, EventArgs e)
+        {
+            List<string> Texts = new List<string>();
+
+            foreach (category categorys in categoryList.Children)
+            {
+
+                string data = "";
+
+                // 每一種資料以"|"分隔加入Texts字串
+                data += categorys.categoryDate.Text + "|" + categorys.categoryName.Text + "|" + categorys.categoryPrice.Text;
+
+                // 存入Texts的陣列
+                Texts.Add(data);
+            }
+         
+        }
+    }
+
+
+
+}
---
+++ WpfApplication1/category.xaml.cs
@@ -0,0 +1,87 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace accounting
+{
+    /// <summary>
+    /// category.xaml 的互動邏輯
+    /// </summary>
+    public partial class category : UserControl
+    {
+        public category()
+        {
+            InitializeComponent();
+        }
+
+        // when click date
+        private void categoryDate_GotFocus(object sender, RoutedEventArgs e)
+        {
+            // delete date
+            categoryDate.Text = "";
+        }
+
+        // when click price
+        private void categoryPrice_GotFocus(object sender, RoutedEventArgs e)
+        {
+            // delete price
+            categoryPrice.Text = "";
+        }
+
+        // when click item
+        private void categoryName_GotFocus(object sender, RoutedEventArgs e)
+        {
+            // delete item
+            categoryName.Text = "";
+        }
+
+        // 使用者沒有點選項目名稱時
+        private void categoryName_LostFocus(object sender, RoutedEventArgs e)
+        {
+            // 如果使用者前一次沒有輸入資料
+            if (categoryName.Text == "")
+            {
+                // 設定回預設內容
+                categoryName.Text = "請輸入項目名稱";
+            }
+
+        }
+        // 使用者沒有點選價格欄位時
+        private void categoryPrice_LostFocus(object sender, RoutedEventArgs e)
+        {
+            // 如果使用者前一次沒有輸入資料
+            if (categoryPrice.Text == "")
+            {
+                // 設定回預設內容
+                categoryPrice.Text = "0";
+            }
+        }
+        // 使用者沒有點選日期欄位時
+        private void categoryDate_LostFocus(object sender, RoutedEventArgs e)
+        {
+            // 如果使用者前一次沒有輸入資料
+            if (categoryDate.Text == "")
+            {
+                // 設定為當前日期
+                categoryDate.Text = DateTime.Now.ToString("M / dd");
+            }
+        }
+
+        private void categoryDate_Loaded(object sender, RoutedEventArgs e)
+        {
+            // 預設日期為當日
+            categoryDate.Text = DateTime.Now.ToString("M / dd");
+        }
+    }
+}
